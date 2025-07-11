8c48093025090228e61e5d08e90ba6926f905b8e
Fernando Cando <fernandocando@hotmail.com>
2025-07-10 19:30:10 -0500

    viernes10

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tracking_ecu/core/theme/app_theme.dart';
 import 'package:tracking_ecu/features/gps/bloc/gps_bloc.dart';
+import 'package:tracking_ecu/features/map/bloc/location_bloc/location_bloc.dart';
 import 'package:tracking_ecu/features/map/bloc/map_cubit/map_cubit.dart';
 import 'package:tracking_ecu/loading/view/loading_page.dart';
-import 'package:tracking_ecu/features/map/bloc/location_bloc/location_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,6 +14,11 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
+  //https://docs.flutter.dev/
+  //https://docs.flutter.dev/codelabs
+  //https://docs.flutter.dev/codelabs
+  //https://github.com/flutter/samples/tree/main/android_splash_screen
+  //https://flutter.dev/docs/development/ui/advanced/splash-screen
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
